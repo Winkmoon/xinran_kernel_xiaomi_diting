1056ac568cdd4a4b8e661a81a30cd5a742de6072
ciallo-0d000721 <2500319550@qq.com>
2025-10-04 19:26:52 +0800

    测试

--- kernel/module.c
+++ kernel/module.c
@@ -3567,7 +3567,9 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 static char *module_blacklist;
 static char *custom_module_blacklist[] = {
     /*sched-walt*/
-	"sched-walt","qcom-cpufreq-hw"
+	"sched-walt",
+	/*qcom-cpufreq-hw*/
+	"qcom-cpufreq-hw"
 };
 
 static bool blacklisted(const char *module_name)
